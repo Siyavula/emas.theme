61513bd2705353a6e3c35058d26d939456cfddb9
Roché Compaan <roche@upfrontsystems.co.za>
2011-12-27 10:45:13 +0200

    convert cnxml file to Message object and use the rfc822 marshaler to initialise object

--- emas/theme/marshal.py
+++ emas/theme/marshal.py
@@ -1,3 +1,5 @@
+import datetime
+from email.Message import Message
 from lxml import etree
 from cStringIO import StringIO
 
@@ -6,9 +8,16 @@ from rwproperty import getproperty, setproperty
 from zope.interface import implements
 from zope.component import adapts
 from zope.filerepresentation.interfaces import IRawWriteFile
+from plone.rfc822 import initializeObjectFromSchemata
 from plone.dexterity.filerepresentation import WriteFileBase
+from plone.dexterity.utils import iterSchemata
 
 from rhaptos.xmlfile.xmlfile import IXMLFile
+
+nsmap = {
+    'cnxml': 'http://cnx.rice.edu/cnxml',
+    'md': 'http://cnx.rice.edu/mdml',
+    }
 
 class CNXMLWriteFile(WriteFileBase):
     """IRawWriteFile file adapter for CNXML files.
@@ -36,10 +45,22 @@ class CNXMLWriteFile(WriteFileBase):
     def close(self):
         self._closed = True
         data = self._getStream().getvalue()
+        tree = etree.fromstring(data)
+        docinfo = tree.getroottree().docinfo
+        msg = Message()
+        for tagname, attrname in (('title', 'title'), 
+                                  ('abstract', 'description'),
+                                  ('created', 'created'),
+                                  ('modified', 'modified')):
+            elems = tree.xpath('//md:%s' % tagname, namespaces=nsmap)
+            if not elems:
+                continue
+            msg[attrname] = elems[0].text
+        msg.set_payload(data)
+        initializeObjectFromSchemata(self.context, iterSchemata(self.context),
+                                     msg, docinfo.encoding)
+
         self._getStream().close()
-        import pdb; pdb.set_trace()
-        doc = etree.fromstring(data)
-         
 
     def _getStream(self):
         return self._file
