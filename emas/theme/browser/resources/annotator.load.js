301b0e152f21f849696e9905ade798fa9a32587d
Izak Burger <isburger@gmail.com>
2011-12-27 23:21:08 +0200

    Load jquery.sub, cleanups
    Load jquery.sub, some cleanups, ignore it if Mathjax is missing.

---
+++ emas/theme/browser/resources/annotator.load.js
@@ -0,0 +1,89 @@
+jQuery(function ($) {
+
+    function maybeWaitForMathJax(fn){
+        /* If mathjax is loaded, wait for it to load, otherwise go right ahead
+           and call our callback */
+        if(typeof MathJax === "undefined"){
+            fn();
+        } else {
+            MathJax.Hub.Queue(fn);
+        }
+    }
+
+  maybeWaitForMathJax(function () {
+    if (typeof $.fn.annotator !== 'function') {
+      alert("Ooops! it looks like you haven't built the Annotator concatenation file. " +
+            "Either download a tagged release from GitHub, or modify the Cakefile to point " +
+            "at your copy of the YUI compressor and run `cake package`.");
+    } else {
+        var elem = $('#content');
+        var account_id = '0b776919ee56436a11fdd72b3f001a23';
+        var annotator_store = 'http://192.168.0.7:5000/api';
+        var userid = null;
+        while (userid == null){
+            userid = prompt("Please enter your dummy user name, for testing","");
+            console.log(userid);
+        }
+        if (userid == ''){
+            userid = "siyavula";
+        }
+        // user name if different from userid (could be ip address if no real user)
+        var options = {};
+        options.permissions = {};
+
+        // don't show the checkboxes
+        options.permissions.showViewPermissionsCheckbox = false
+        options.permissions.showEditPermissionsCheckbox = false
+        options.permissions.user = {
+            'name': userid
+        };
+        options.categories = {
+            'errata':'annotator-hl-errata', 
+            'comment':'annotator-hl-comment', 
+            'suggestion':'annotator-hl-suggestion'
+        };
+
+        if(userid) {
+            elem.data('annotator:headers', {'x-annotator-user-id': userid});
+            // set user
+            options.permissions.user.id = userid;
+            // configure permissions (only owner can edit, everyone can read)
+            options.permissions.permissions = {
+                'read': [],
+                'update': [userid],
+                'delete': [userid],
+                'admin': [userid]
+            }
+        }
+        var annotator = elem.annotator().data('annotator');
+        annotator.addPlugin('Store', {
+            prefix: annotator_store,
+            annotationData: {
+                'uri':  'http://192.168.0.7/cnxdemo/pr01.html',
+                // set account id
+                'account_id': account_id
+            },
+            loadFromSearch: {
+                'uri': 'http://192.168.0.7/cnxdemo/pr01.html',
+                limit: -1 
+            }
+        });
+        // when loading annotations from backend determine how to get the user id
+        options.permissions.userId = function(user) {
+            if (user) {
+                return user.id;
+            }
+        };
+        // ditto for human readable string for user
+        options.permissions.userString = function(user) {
+            if (user) {
+                return user.name;
+            }
+        };
+        annotator.addPlugin('Permissions', options.permissions);
+        annotator.addPlugin('Categories', options.categories);
+        annotator.addPlugin('RoundupStatus');
+        annotator.addPlugin('Markdown');
+    }
+  });
+});
