5378a09b6289e79baab8801df89de947de990d00
Rijk Stofberg <rijk.stofberg@gmail.com>
2011-12-29 16:48:29 +0200

    Update tests to check for input and output mimetypes. Also done some renaming.

--- emas/theme/tests/test_transforms.py
+++ emas/theme/tests/test_transforms.py
@@ -5,9 +5,10 @@ from base import INTEGRATION_TESTING
 from Products.PortalTransforms.data import datastream
 from Products.CMFCore.utils import getToolByName
 
-from emas.theme.transforms.cnxmlplus2cnxml import cnxmlplus_to_cnxml
+from emas.theme.transforms.cnxmlplus2shortcodecnxml import cnxmlplus_to_shortcodecnxml
 from emas.theme.transforms.shortcodehtml2html import shortcodehtml_to_html
-from rhaptos.cnxmltransforms.cnxml2html import cnxml_to_html
+from emas.theme.transforms.shortcodecnxml2shortcodehtml import \
+    shortcodecnxml_to_shortcodehtml
 
 dirname = os.path.dirname(__file__)
 
@@ -17,18 +18,45 @@ class TestTransforms(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
+    
+    def test_cnxmlplus2shortcodecnxml_iotypes(self):
+        transform = self.portal.portal_transforms['cnxmlplus_to_shortcodecnxml']
+        self.assertEqual(
+            transform.inputs[0], 'application/cnxmlplus+xml',
+            'test_cnxmlplus2cnxml_iotypes: Input type wrong')
+        self.assertEqual(
+            transform.output, 'application/shortcodecnxml+xml',
+            'test_cnxmlplus2cnxml_iotypes: Output type wrong')
 
     def test_cnxmlplus2cnxml(self):
         cnxmlplus = open(os.path.join(dirname, 'test.cnxmlplus')).read()
-        transform = cnxmlplus_to_cnxml()
+        transform = cnxmlplus_to_shortcodecnxml()
         data = datastream('cnxml')
         data = transform.convert(cnxmlplus, data) 
 
-    def test_cnxml2shortcodehtml(self):
+    def test_shortcodecnxml2shortcodehtml_iotypes(self):
+        transform = self.portal.portal_transforms['shortcodecnxml_to_shortcodehtml']
+        self.assertEqual(
+            transform.inputs[0], 'application/shortcodecnxml+xml',
+            'test_shortcodecnxml2shortcodehtml_iotypes: Input type wrong')
+        self.assertEqual(
+            transform.output, 'text/shortcodehtml',
+            'test_cnxml2shortcodehtml_iotypes: Output type wrong')
+
+    def test_shortcodecnxml2shortcodehtml(self):
         cnxml = open(os.path.join(dirname, 'test.cnxml')).read()
-        transform = cnxml_to_html()
+        transform = shortcodecnxml_to_shortcodehtml()
         data = datastream('cnxml')
         data = transform.convert(cnxml, data) 
+
+    def test_shortcodehtml2html_iotypes(self):
+        transform = self.portal.portal_transforms['shortcodehtml_to_html']
+        self.assertEqual(
+            transform.inputs[0], 'text/shortcodehtml',
+            'test_shortcodehtml2html_iotypes: Input type wrong')
+        self.assertEqual(
+            transform.output, 'text/html',
+            'test_shortcodehtml2html_iotypes: Output type wrong')
 
     def test_shortcodehtml2html(self):
         html = open(os.path.join(dirname, 'test.html')).read()
@@ -36,7 +64,16 @@ class TestTransforms(unittest.TestCase):
         data = datastream(html)
         data = transform.convert(html, data) 
 
-    def test_cnxmlplus2html(self):
+    def test_cnxmlplus2html_iotypes(self):
+        transform = self.portal.portal_transforms['cnxmlplus_to_html_chain']
+        self.assertEqual(
+            transform.inputs[0], 'application/cnxmlplus+xml',
+            'test_cnxmlplus2html_iotypes: Input type wrong')
+        self.assertEqual(
+            transform.output, 'text/html',
+            'test_cnxmlplus2html_iotypes: Output type wrong')
+
+    def _test_cnxmlplus2html_chain(self):
         """ This looks more like a functional than unittest since it exercises
             all the others too. Maybe we should move it.
         """
