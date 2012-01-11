1c8599d639ed02dabb23512c509e4b30485189b4
Rijk Stofberg <rijk.stofberg@gmail.com>
2012-01-11 10:40:33 +0200

    Extension to add attribute for next/previous nav restriction

---
+++ emas/theme/extenders/navigation.py
@@ -0,0 +1,34 @@
+from zope.interface import implements
+
+from Products.Archetypes.Widget import BooleanWidget
+from Products.Archetypes.public import BooleanField
+
+from archetypes.schemaextender.field import ExtensionField
+from archetypes.schemaextender.interfaces import ISchemaExtender
+
+import logging
+LOG = logging.getLogger('NaviationExtender')
+
+class _NavigationRestrictionExtensionField(ExtensionField, BooleanField): pass
+
+class NavigationRestrictionsExtender(object):
+    implements(ISchemaExtender)
+
+    fields = [
+        _NavigationRestrictionExtensionField(
+            "restrictNextPreviousNavigation",
+            default = False,
+            widget = BooleanWidget(
+                label=u"Restrict navigation",
+                description=u"Limit next/previous navigation to this folder and its children.",
+            ),
+            schemata='settings',
+        ),
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        return self.fields
+
