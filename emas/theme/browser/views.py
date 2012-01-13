ae8b7f8605093f44fe745f733544280247d72da4
Roché Compaan <roche@upfrontsystems.co.za>
2012-01-13 18:22:06 +0200

    integrate shortcode lookup with search

--- emas/theme/browser/views.py
+++ emas/theme/browser/views.py
@@ -1,9 +1,16 @@
 from zope.component import queryUtility
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
 from plone.app.layout.viewlets.common import ViewletBase
 from plone.registry.interfaces import IRegistry
 from plone.app.registry.browser.controlpanel import RegistryEditForm
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from upfront.shorturl.browser.views import RedirectView
+
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
 from emas.theme.interfaces import IEmasSettings
 from emas.theme import MessageFactory as _
 
@@ -38,3 +45,21 @@ class AnnotatorConfigViewlet(ViewletBase):
 class AnnotatorHelpViewlet(ViewletBase):
     """ Adds a help panel for the annotator. """
     index = ViewPageTemplateFile('annotatorhelp.pt')
+
+
+class SearchView(BrowserView):
+    """ Combine searching for shortcode and searchabletext
+    """
+
+    def __call__(self):
+        searchtext = self.request.get('SearchableText')
+        shortcodeview = RedirectView(self.context, self.request)
+        target = shortcodeview.lookup(searchtext)
+        if target:
+            self.request.response.redirect(target)
+        else:
+            state = self.context.restrictedTraverse('@@plone_portal_state')
+            root = state.navigation_root()
+            search_url = '%s/search?SearchableText=%s' % (
+                root.absolute_url(), searchtext)
+            self.request.response.redirect(search_url)
