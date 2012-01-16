a5cb6181e7995dc5bc5f865c498f2253848e4b7c
Rijk Stofberg <rijk.stofberg@gmail.com>
2012-01-16 23:40:55 +0200

    Add related content behavior and use it in question

--- emas/theme/behaviors/relatedcontentbehavior.py
+++ emas/theme/behaviors/relatedcontentbehavior.py
@@ -1,44 +1,21 @@
-from zope.interface import alsoProvides, implements
-from zope.component import adapts
-from zope import schema
+from zope.interface import alsoProvides
 from plone.directives import form
-from plone.dexterity.interfaces import IDexterityContent
 from plone.autoform.interfaces import IFormFieldProvider
 
-from plone.namedfile import field as namedfile
-from z3c.relationfield.schema import RelationChoice, RelationList
+from z3c.relationfield.schema import RelationChoice
 from plone.formwidget.contenttree import ObjPathSourceBinder
 
 from emas.theme import MessageFactory as _
 
 
-class IRelatedcontentbehavior(form.Schema):
+class IRelatedContent(form.Schema):
     """
        Marker/Form interface for Related content behavior
     """
-   
-    # -*- Your Zope schema definitions here ... -*-
+    relatedContent = RelationChoice(
+        title=_(u'label_content_item', default=u'Related Content'),
+        source=ObjPathSourceBinder(),
+        required=False,
+    )
 
-
-alsoProvides(IRelatedcontentbehavior,IFormFieldProvider)
-
-def context_property(name):
-    def getter(self):
-        return getattr(self.context, name)
-    def setter(self, value):
-        setattr(self.context, name, value)
-    def deleter(self):
-        delattr(self.context, name)
-    return property(getter, setter, deleter)
-
-class Relatedcontentbehavior(object):
-    """
-       Adapter for Related content behavior
-    """
-    implements(IRelatedcontentbehavior)
-    adapts(IDexterityContent)
-
-    def __init__(self,context):
-        self.context = context
-
-    # -*- Your behavior property setters & getters here ... -*-
+alsoProvides(IRelatedContent,IFormFieldProvider)
