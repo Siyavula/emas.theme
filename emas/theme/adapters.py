21774abb0cebd2c7dfb6a7e521b3fd5beac08df9
Roché Compaan <roche@upfrontsystems.co.za>
2012-01-19 13:03:12 +0200

    additional user fields

--- emas/theme/adapters.py
+++ emas/theme/adapters.py
@@ -1,6 +1,28 @@
 from plone.app.users.browser.personalpreferences import UserDataPanelAdapter
 
 class EmasUserDataPanelAdapter(UserDataPanelAdapter):
-    credits = property(
-        lambda s: s.context.getProperty('credits'),
-        lambda s, v: s.context.setProperties({'credits': v}))
+
+    def get_credits(self):
+        return self.context.getProperty('credits', 0)
+    def set_credits(self, value):
+        return self.context.setMemberProperties({'credits': value})
+    credits = property(get_credits, set_credits)
+
+    def get_userrole(self):
+        return self.context.getProperty('userrole', '')
+    def set_userrole(self, value):
+        return self.context.setMemberProperties({'userrole': value})
+    userrole = property(get_userrole, set_userrole)
+
+    def get_school(self):
+        return self.context.getProperty('school', '')
+    def set_school(self, value):
+        return self.context.setMemberProperties({'school': value})
+    school = property(get_school, set_school)
+
+    def get_province(self):
+        return self.context.getProperty('province', '')
+    def set_province(self, value):
+        return self.context.setMemberProperties({'province': value})
+    province = property(get_province, set_province)
+
