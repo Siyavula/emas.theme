9ebb1022eec86c9d837ddfe3ff5a3873b0d8d2bf
Izak Burger <isburger@gmail.com>
2012-01-26 12:27:20 +0200

    add new dexterity type for transactions

---
+++ emas/theme/transaction.py
@@ -0,0 +1,15 @@
+from zope.schema import Datetime, Int
+from plone.directives import form, dexterity
+from emas.theme import MessageFactory as _
+
+class ITransaction(form.Schema):
+    """ A transaction, a user either bought credits, or spent credits on a
+       service. """
+
+    date = Datetime(
+        title=_(u"The date and time of the transaction")
+    )
+
+    amount = Int(
+        title=_(u"The amount of the transaction")
+    )
