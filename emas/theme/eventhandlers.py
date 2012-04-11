e175c515af796ff2477c55a117396f651f92de8e
Roché Compaan <roche@upfrontsystems.co.za>
2012-04-11 11:57:43 +0200

    register user for 30 day trial with 2 questions

--- emas/theme/eventhandlers.py
+++ emas/theme/eventhandlers.py
@@ -22,10 +22,15 @@ def onMemberJoined(obj, event):
         # Finally, change its permissions
         folder.manage_permission(ModifyPortalContent, roles=[], acquire=0)
     
+
+    # 30 days free trial with 2 questions
+    trialend = datetime.date.today() + datetime.timedelta(days=30)
+
     # Make sure the user's service registration dates are correct
-    properties = {'askanexpert_registrationdate': NULLDATE,
-                  'answerdatabase_registrationdate': NULLDATE,
-                  'moreexercise_registrationdate': NULLDATE,
+    properties = {'askanexpert_registrationdate': trialend,
+                  'answerdatabase_registrationdate': trialend,
+                  'moreexercise_registrationdate': trialend,
+                  'credits': 2,
                  }
     propsheet = obj.getPropertysheet('mutable_properties')
     for key, value in properties.items():
@@ -39,4 +44,6 @@ def questionAsked(obj, event):
 
     member = obj.restrictedTraverse('@@plone_portal_state').member()
     credits = member.getProperty('credits') - 1
+    if credits < 0:
+        raise RuntimeError("Credits can't be less than zero")
     member.setMemberProperties({'credits': credits})
