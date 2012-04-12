598249084ee1e154bc3207e6c35e954a8a00afb8
Roché Compaan <roche@upfrontsystems.co.za>
2012-04-12 12:42:19 +0200

    submit order

--- emas/theme/browser/order.py
+++ emas/theme/browser/order.py
@@ -52,10 +52,15 @@ class OrderForm(BrowserView):
 
             self.totalcost = totalcost
             self.packages = packages
+            state = self.context.restrictedTraverse('@@plone_portal_state')
+            self.username = state.member().getId()
+            self.send()
 
-        else:
-            return self.index()
-            
+        return self.index()
+
+    def ordersubmitted(self):
+        return self.request.has_key('ordersubmitted')
+
     def send(self): 
         """ Send Invoice to recipients
         """
@@ -72,22 +77,23 @@ class OrderForm(BrowserView):
             ( 'Siyavula Education', self.settings.order_email_address )
         )
 
-        send_to_address = formataddr((member.fullname, member.email))
+        send_to_address = formataddr((member.getProperty('fullname'),
+                                      member.getProperty('email')))
 
         subject = 'Order from Everything %s Website' % \
             state.navigation_root_title()
 
         # Generate message and attach to mail message
         message = self.ordertemplate(
-            fullname=member.fullname,
+            fullname=member.getProperty('fullname'),
             sitename='Everything %s' % state.navigation_root_title(),
             packages=self.packages,
-            cost=self.totalcost,
+            totalcost=self.totalcost,
             username=member.getId(),
             email=self.settings.order_email_address,
             phone=self.settings.order_phone_number
         )
 
-        portal.MailHost.send(message, send_from_address, send_to_address,
+        portal.MailHost.send(message, send_to_address, send_from_address,
                              subject)
 
