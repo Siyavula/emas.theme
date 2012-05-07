a44f3ea9f82c86360dba09ff0f1a3ef1a97821fa
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-05-07 15:31:08 +0200

    Begin unifying qaviewlet in theme too.

--- emas/theme/browser/viewlets.py
+++ emas/theme/browser/viewlets.py
@@ -2,17 +2,17 @@ from Products.Archetypes.utils import shasattr
 
 from Products.CMFCore.utils import getToolByName
 from plone.uuid.interfaces import IUUID
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-from siyavula.what.browser.questionslistviewlet \
-    import QuestionsListViewlet as BaseQuestionsListViewlet
+from siyavula.what.browser.viewlets import QAViewlet as BaseQAViewlet
 from emas.theme.browser.views import is_expert
-
 from emas.theme import MessageFactory as _
 
 
-class QuestionsListViewlet(BaseQuestionsListViewlet):
+class QAViewlet(BaseQAViewlet):
     """ Specialise the siyavula.what viewlet to check if the service is enabled.
     """
+    index = ViewPageTemplateFile('templates/addquestion.pt')
 
     def questions(self):
         """ Return all questions that have the current context set
@@ -42,3 +42,28 @@ class QuestionsListViewlet(BaseQuestionsListViewlet):
             query['Creator'] = member.getId()
         brains = pc(query)
         return brains and [b.getObject() for b in brains] or []
+
+    def allowQuestions(self):
+        """ Check against the members enabled services.
+        """
+        context = self.context
+        allowQuestions = False
+        if shasattr(context, 'allowQuestions'):
+            allowQuestions = getattr(context, 'allowQuestions')
+        return allowQuestions
+
+    def is_enabled(self):
+        """
+        """
+        context = self.context
+        view = context.restrictedTraverse('@@enabled-services')
+        return view.ask_expert_enabled
+
+    def render(self):
+        """ We render an empty string when a specific piece of content
+            does not allow questions.
+        """
+        if self.allowQuestions() and self.is_enabled():
+            return super(QuestionAddViewlet, self).render()
+        else:
+            return ""
