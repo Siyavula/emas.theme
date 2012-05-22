e0d9a57a086d82c5bbc23bafeec613c7334b07d9
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-05-22 14:43:37 +0200

    Ad mockmail and grade indicators.

--- emas/theme/tests/test_order.py
+++ emas/theme/tests/test_order.py
@@ -3,7 +3,11 @@ import unittest2 as unittest
 from plone.app.testing import TEST_USER_ID
 from base import INTEGRATION_TESTING
 
+from zope.component import getSiteManager
+from Products.CMFPlone.tests.utils import MockMailHost
+from Products.MailHost.interfaces import IMailHost
 from AccessControl import getSecurityManager
+
 
 class TestOrderForm(unittest.TestCase):
     """ Test the nextprevious adapter """
@@ -11,6 +15,15 @@ class TestOrderForm(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
+        self.setupMailHost()
+        state = self.portal.restrictedTraverse('@@plone_portal_state')
+        member = state.member()
+        member.setProperties({'fullname': 'Tester One',
+                              'email': 'testerone@example.com'})
+
+
+    def beforeTearDown(self):
+        self.restoreMailHost()
 
     def test_orderform(self):
         orderform = self.portal.restrictedTraverse('@@order') 
@@ -22,45 +35,49 @@ class TestOrderForm(unittest.TestCase):
         request = self.layer['request']
         request["submit"] = True
         request["practice_subjects"] = "Maths"
+        request["practice_grade"] = "Grade 10"
         request["include_textbook"] = "no"
         request["include_expert_answers"] = "no"
 
         orderform()
         self.assertEqual(orderform.totalcost, 150)
         self.assertEqual(orderform.packages,
-            [u'1 year subscription to Intelligent Practice for Maths'])
+            [u'1 year subscription to Intelligent Practice for Maths Grade 10'])
 
         # place an order for maths and textbook
         request = self.layer['request']
         request["submit"] = True
         request["practice_subjects"] = "Maths"
+        request["practice_grade"] = "Grade 10"
         request["include_textbook"] = "yes"
         request["include_expert_answers"] = "no"
 
         orderform()
         self.assertEqual(orderform.totalcost, 200)
         self.assertEqual(orderform.packages,
-            [u'1 year subscription to Intelligent Practice for Maths',
-             u'Printed textbook for Maths'])
+            [u'1 year subscription to Intelligent Practice for Maths Grade 10',
+             u'Printed textbook for Maths Grade 10'])
 
         # place an order for science and expert answers
         request = self.layer['request']
         request["submit"] = True
         request["practice_subjects"] = "Science"
+        request["practice_grade"] = "Grade 10"
         request["include_textbook"] = "no"
         request["include_expert_answers"] = "yes"
 
         orderform()
         self.assertEqual(orderform.totalcost, 175)
         self.assertEqual(orderform.packages,
-            [u'1 year subscription to Intelligent Practice for Science',
-             u'Expert answers 10 of your questions'])
+            [u'1 year subscription to Intelligent Practice for Science Grade 10',
+             u'Expert answers to 10 of your questions'])
 
 
         # place an order for maths and science only
         request = self.layer['request']
         request["submit"] = True
         request["practice_subjects"] = "Maths,Science"
+        request["practice_grade"] = "Grade 10"
         request["include_textbook"] = "no"
         request["include_expert_answers"] = "no"
 
@@ -68,12 +85,13 @@ class TestOrderForm(unittest.TestCase):
         self.assertEqual(orderform.totalcost, 250)
         self.assertEqual(orderform.packages,
             [(u'1 year subscription to Intelligent Practice for '
-                'Maths and Science')])
+                'Maths and Science Grade 10')])
 
         # place an order for everything
         request = self.layer['request']
         request["submit"] = True
         request["practice_subjects"] = "Maths,Science"
+        request["practice_grade"] = "Grade 10"
         request["include_textbook"] = "yes"
         request["include_expert_answers"] = "yes"
 
@@ -81,7 +99,21 @@ class TestOrderForm(unittest.TestCase):
         self.assertEqual(orderform.totalcost, 375)
         self.assertEqual(orderform.packages,
             [(u'1 year subscription to Intelligent Practice for '
-                'Maths and Science'),
-             u'Printed textbook for Maths and Science',
-             u'Expert answers 10 of your questions',
+                'Maths and Science Grade 10'),
+             u'Printed textbook for Maths and Science Grade 10',
+             u'Expert answers to 10 of your questions',
             ])
+
+    def setupMailHost(self):
+        self.portal._original_MailHost = self.portal.MailHost
+        self.portal.MailHost = mailhost = MockMailHost('MailHost')
+        sm = getSiteManager(context=self.portal)
+        sm.unregisterUtility(provided=IMailHost)
+        sm.registerUtility(mailhost, provided=IMailHost)
+
+    def restoreMailHost(self):
+        self.portal.MailHost = self.portal._original_MailHost
+        sm = getSiteManager(context=self.portal)
+        sm.unregisterUtility(provided=IMailHost)
+        sm.registerUtility(aq_base(self.portal._original_MailHost), provided=IMailHost)
+
