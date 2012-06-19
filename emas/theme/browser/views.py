0c7eae925497b5d756d481d42b3820e4d03496e6
Ewald Zietsman <ewald@siyavula.org.za>
2012-06-19 15:51:12 +0200

    Added email notification for annotator reply system

--- emas/theme/browser/views.py
+++ emas/theme/browser/views.py
@@ -112,6 +112,9 @@ class AnnotatorConfigViewlet(ViewletBase):
     def annotatorStore(self):
         return self.settings.store
 
+    def bccAddress(self):
+        return self.settings.bcc_address
+    
     def userId(self):
         return self.portal_state.member().getId()
 
