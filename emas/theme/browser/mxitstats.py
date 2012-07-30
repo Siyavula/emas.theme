b6afefc07fa89c0404b0917b255e1c52621d2f5b
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-07-30 12:25:17 +0200

    Add the first little bit of a stats view

---
+++ emas/theme/browser/mxitstats.py
@@ -0,0 +1,31 @@
+from five import grok
+from zope.interface import Interface
+from Products.CMFCore.utils import getToolByName
+
+from pas.plugins.mxit.plugin import member_id
+from pas.plugins.mxit.plugin import USER_ID_TOKEN
+
+from emas.theme.browser.mxitpayment import SUBJECT_MAP
+from emas.theme.interfaces import IEmasThemeLayer
+
+grok.templatedir('templates')
+grok.layer(IEmasThemeLayer)
+
+
+class MXitStats(grok.View):
+    """
+        Custom view for mxit signup stats 
+    """
+
+    grok.context(Interface)
+    grok.require('zope2.View')
+    
+    def stats_per_group(self):
+        group_stats = {}
+        gt = getToolByName(self.context, 'portal_groups')
+        for groupname in SUBJECT_MAP.values():
+            group = gt.getGroupById(groupname)
+            count = len(group.getMemberIds())
+            group_stats[groupname] = count
+
+        return group_stats
