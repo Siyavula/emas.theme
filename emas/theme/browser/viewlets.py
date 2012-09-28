7d9dd2f0a66463a92b454adf6b873e8a7f9a2faa
Roché Compaan <roche@upfrontsystems.co.za>
2012-09-28 16:59:44 +0200

    use same config as TeX-AMS-MML_HTMLorMML.js

--- emas/theme/browser/viewlets.py
+++ emas/theme/browser/viewlets.py
@@ -110,7 +110,9 @@ class MathJaxViewlet(ViewletBase):
 <script type="text/x-mathjax-config">
 MathJax.Ajax.timeout = 60*1000;
 MathJax.Hub.Config({
-    config: ["TeX-AMS-MML_HTMLorMML.js"],
+    config: ["MMLorHTML.js"],
+    extensions: ["tex2jax.js","mml2jax.js","MathZoom.js","MathMenu.js","toMathML.js","TeX/noErrors.js","TeX/noUndefined.js","TeX/AMSmath.js","TeX/AMSsymbols.js"],
+    jax: ["input/TeX","input/MathML","output/HTML-CSS","output/NativeMML"]
 });
 </script>
 <script type="text/javascript" src="%s/++resource++fullmarks.mathjax/mathjax/MathJax.js">
