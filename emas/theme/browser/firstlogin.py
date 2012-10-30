6b2cacbd9d2b3f6e601d99dd670112aeb5fd8a90
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-10-30 08:49:29 +0200

    First commit

---
+++ emas/theme/browser/firstlogin.py
@@ -0,0 +1,18 @@
+from five import grok
+from zope.interface import Interface
+
+grok.templatedir('templates')
+
+
+class FirstLogin(grok.View):
+    """ Confirm an order.
+    """
+    
+    grok.context(Interface)
+    grok.require('zope2.View')
+
+    def practice_url(self):
+        import pdb;pdb.set_trace()
+        pps = self.context.restrictedTraverse('@@plone_portal_state')
+        navroot = pps.navigation_root().absolute_url()
+        return '%s/@@practice' % navroot
