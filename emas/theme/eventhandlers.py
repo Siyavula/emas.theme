a4dcf99040612a3f24b04f5c55d3487dac7c905d
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-11-21 11:05:55 +0200

    Add eventhandler and registration to subscribe new members to the newsletter.

--- emas/theme/eventhandlers.py
+++ emas/theme/eventhandlers.py
@@ -1,7 +1,44 @@
+import logging
+
 from Products.CMFCore.utils import getToolByName
 
 
+LOGGER = logging.getLogger('emas.theme:eventhandlers:')
+
 def onUserInitialLogin(obj, event):
+    set_welcome_message(obj, event)
+    subscribe_to_newsletters(obj, event)
+
+def set_welcome_message(obj, event):
     plone_utils = getToolByName(obj, 'plone_utils')
     message = obj.restrictedTraverse('@@firstlogin')()
     plone_utils.addPortalMessage(message, 'info')
+
+def subscribe_to_newsletters(obj, event):
+    subscribe_prefix = 'subscribe_to_'
+
+    portal = obj.restrictedTraverse('@@plone_portal_state').portal()
+    newsletters = portal._getOb('newsletters')
+
+    # get a list of available newsletters
+    newsletter_names = newsletters.objectIds()
+    # if we don't have newsletters configured, warn and carry on.
+    if not newsletter_names:
+        LOGGER.warn('No newsletters configured')
+        return
+
+    for name in newsletter_names:
+        # subscribe the user to each one that he accepted during registration
+        if obj.getProperty('%s%s' % (subscribe_prefix, name), False):
+            portal.REQUEST['subscriber'] = obj.getProperty('email')
+            portal.REQUEST['fullname'] = obj.getProperty('fullname')
+            portal.REQUEST['salutation'] = obj.getProperty('salutation', '')
+            portal.REQUEST['organisation'] = obj.getProperty('school')
+            nl_path = '/'.join(newsletters._getOb(name).getPhysicalPath())
+            portal.REQUEST['newsletter'] = nl_path
+
+            view = portal.restrictedTraverse('@@register-subscriber')
+            view()
+
+def onMemberPropsUpdated(obj, event):
+    subscribe_to_newsletters(obj, event)
