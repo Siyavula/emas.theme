6eae72e293015f579366f6c1f2abc85f77ba4e9e
Roché Compaan <roche@upfrontsystems.co.za>
2013-03-07 11:05:09 +0200

    allow anonymous requests to @@practice/media

--- emas/theme/browser/practice.py
+++ emas/theme/browser/practice.py
@@ -65,7 +65,8 @@ class Practice(BrowserView):
 
         portal_state = self.context.restrictedTraverse('@@plone_portal_state')
         path = self.request.get_header('PATH_INFO')
-        is_static = path and '@@practice/static' in path
+        is_static = path and ('@@practice/static' in path or
+                              '@@practice/media' in path)
         if portal_state.anonymous() and not is_static:
             return self.request.RESPONSE.unauthorized()
 
