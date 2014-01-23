be591a90034f6f3cf8adc597b2520ac4252904cb
Roché Compaan <roche@upfrontsystems.co.za>
2014-01-23 15:57:34 +0200

    revert last commit

--- emas/theme/browser/views.py
+++ emas/theme/browser/views.py
@@ -808,7 +808,4 @@ class EMASPortalMessage(BrowserView):
     index = ViewPageTemplateFile('templates/portalmessage.pt')
 
     def __call__(self):
-        self.messages = []
-        if hasattr(self.context, 'plone_utils'):
-            self.messages = self.context.plone_utils.showPortalMessages()
         return self.index()
