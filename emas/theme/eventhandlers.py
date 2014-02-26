93e67f4aa77bd7248520f16857ecdba8e882d5a7
Roché Compaan <roche@upfrontsystems.co.za>
2014-02-26 16:50:28 +0200

    move the user catalog to emas.app

--- emas/theme/eventhandlers.py
+++ emas/theme/eventhandlers.py
@@ -2,8 +2,6 @@ import logging
 from DateTime import DateTime
 from types import ListType
 from zope.component.hooks import getSite
-from zope.component import getUtility
-from emas.theme.usercatalog import IUserCatalog
 from Products.CMFCore.utils import getToolByName
 
 from emas.theme.userdataschema import IEmasUserDataSchema
@@ -24,11 +22,9 @@ def onUserInitialLogin(obj, event):
     set_welcome_message(obj, event)
     update_newsletter_subscription(obj, event)
     store_registration_date(obj, event)    
-    getUtility(IUserCatalog).index(obj)
 
 def onMemberPropsUpdated(obj, event):
     update_newsletter_subscription(obj, event)
-    getUtility(IUserCatalog).index(obj)
 
 def set_welcome_message(obj, event):
     plone_utils = getToolByName(obj, 'plone_utils')
